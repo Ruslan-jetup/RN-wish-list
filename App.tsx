c451e0627b027cf77e5c2869bd793bdd341bf06a
Ruslan Nikolaiets <mnemonikus69@gmail.com>
2024-12-20 15:52:11 +0200

    FEATURE | SplashScreen  and app icon added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
+import React, { useEffect } from 'react';
 import { Navigation } from 'modules';
-import React from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BootSplash from 'react-native-bootsplash';
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    BootSplash.hide({ fade: true });
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Navigation />
